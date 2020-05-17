d6861022ad51c939b5a81120249960a57989639b
enriqueMontano <montanoenrique@outlook.com>
2020-05-17 16:46:28 +0200

    Add basic project structure

---
+++ src/components/details/page.js
@@ -0,0 +1,5 @@
+import React, { Fragment } from "react";
+
+const Page = (props) => <Fragment>Details</Fragment>;
+
+export default Page;
---
+++ src/components/results/page.js
@@ -0,0 +1,5 @@
+import React, { Fragment } from "react";
+
+const Page = (props) => <Fragment>Results</Fragment>;
+
+export default Page;
