2c54f7060a1529e7a15f64a7ecb43f8e73d2a104
enriqueMontano <montanoenrique@outlook.com>
2020-05-18 23:53:10 +0200

    Add Autocomplete component to navbar

--- src/components/appBar/page.js
+++ src/components/appBar/page.js
@@ -1,11 +1,20 @@
 import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
+import Autocomplete from "../autocomplete";
+import "./style.css";
 
 function Page(props) {
   return (
     <AppBar position="static">
-      <Toolbar className="appbar"></Toolbar>
+      <Toolbar className="appbar">
+        <Autocomplete
+          text={""}
+          suggestions={[]}
+          onChangeText={(text) => {}}
+          onChangeSelections={(text) => {}}
+        />
+      </Toolbar>
     </AppBar>
   );
 }
